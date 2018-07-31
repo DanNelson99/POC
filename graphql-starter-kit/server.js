201d724a97a7e1b872089f001491b2486907bf32
DanNelson9999 <dan.nelson.fred@gmail.com>
2018-07-31 15:13:02 -0300

    Add apollo graphgl

--- graphql-starter-kit/server.js
+++ graphql-starter-kit/server.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use(compression());
+graphQLServer.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    // This option turns on tracing
+    tracing: false,
+    cacheControl: false
+  })
+);
+
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+//export default graphQLServer;
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
   console.log(
